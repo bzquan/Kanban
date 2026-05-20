ac8dc55c74e46e4dc49a0402c43059cf4fc3995f
FST2507010\Zen <bzquan@gmail.com>
2026-05-20 16:20:42 +0900

    DragAndDropLibをUtilへコピー

---
+++ Util/DragAndDropLib/ForbidDragAndDropRequestArg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragAndDropLib
+{
+    /// <summary>
+    /// 「カード内容の編集後、カード順が勝手に変更される」に対応するため、DragAndDrop制御用イベントを追加した。
+    /// 障害原因：
+    ///   DragAndDropLib.ItemsControlDragDropDecoratorはMouseDown, MouseMove, MouseUpにてDragAndDropを制御する。
+    ///   ところが、mouse double click時、MouseDownが先勝つが、EditCardDialogを素早く表示すると、
+    ///   MouseUpが処理できず、EditCardDialogを閉じるためのクリックのMouseUpに反応され、Cardが移動されたと誤認識される。
+    /// </summary>
+
+    public class DisallowDragAndDropRequestArg : EventArgs
+    {
+    }
+
+    public class AllowDragAndDropRequestArg : EventArgs
+    {
+    }
+}
