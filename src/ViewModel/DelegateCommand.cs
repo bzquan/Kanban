46db3273b2aa55b336ff849841fee8d58c282e50
FST2507010\Zen <bzquan@gmail.com>
2026-05-28 14:11:24 +0900

    nullについてのコンパイル警告に対応

--- src/ViewModel/DelegateCommand.cs
+++ src/ViewModel/DelegateCommand.cs
@@ -1,41 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
-namespace Kanban.ViewModel
+namespace Kanban.ViewModel;
+
+public class DelegateCommand<T> : ICommand
 {
-    public class DelegateCommand<T> : ICommand
+    private readonly Action<T>? _execute = null;
+    private readonly Predicate<T>? _canExecute = null;
+
+    public event EventHandler? CanExecuteChanged;
+
+    #region Constructors
+    public DelegateCommand(Action<T>? execute)
+        : this(execute, null)
     {
-        readonly Action<T> _execute = null;
-        readonly Predicate<T> _canExecute = null;
-
-        #region Constructors
-        public DelegateCommand(Action<T> execute)
-            : this(execute, null)
-        {
-        }
-
-        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
-        {
-            if (execute == null)
-                throw new ArgumentNullException("execute");
-
-            _execute = execute;
-            _canExecute = canExecute;
-        }
-
-        #endregion
-
-        #region ICommand Members
-
-        public bool CanExecute(object parameter) => _canExecute?.Invoke((T)parameter) ?? true;
-        public void Execute(object parameter) => _execute((T)parameter);
-        public event EventHandler CanExecuteChanged;
-        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-
-        #endregion
     }
+
+    public DelegateCommand(Action<T>? execute, Predicate<T>? canExecute)
+    {
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    #endregion
+
+    #region ICommand Members
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute?.Invoke((T)parameter) ?? true;
+    }
+
+    public void Execute(object? parameter) => _execute((T)parameter);
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    #endregion
 }
--- src/ViewModel/NotifyPropertyChangedBase.cs
+++ src/ViewModel/NotifyPropertyChangedBase.cs
@@ -1,35 +1,33 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Kanban.ViewModel
+namespace Kanban.ViewModel;
+
+public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
 {
-    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(
+    [CallerMemberName] string? propertyName = null)
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
-        protected virtual void OnPropertyChanged(
-        [CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    protected virtual bool SetProperty<T>(ref T item, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(item, value)) return false;
+        item = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
 
-        protected virtual bool SetProperty<T>(ref T item, T value, [CallerMemberName] string propertyName = null)
+    protected void SetProperty<TTarget, TValue>(TTarget target, TValue newValue, [CallerMemberName] string? targetPropertyName = null, [CallerMemberName] string propertyName = null)
+    {
+        System.Reflection.PropertyInfo prop = target.GetType().GetProperty(targetPropertyName);
+        if (!EqualityComparer<TValue>.Default.Equals((TValue)prop.GetValue(target), newValue))
         {
-            if (EqualityComparer<T>.Default.Equals(item, value)) return false;
-            item = value;
+            prop.SetValue(target, newValue);
             OnPropertyChanged(propertyName);
-            return true;
-        }
-
-        protected void SetProperty<TTarget, TValue>(TTarget target, TValue newValue, [CallerMemberName] string targetPropertyName = null, [CallerMemberName] string propertyName = null)
-        {
-            System.Reflection.PropertyInfo prop = target.GetType().GetProperty(targetPropertyName);
-            if (!EqualityComparer<TValue>.Default.Equals((TValue)prop.GetValue(target), newValue))
-            {
-                prop.SetValue(target, newValue);
-                OnPropertyChanged(propertyName);
-            }
         }
     }
 }
